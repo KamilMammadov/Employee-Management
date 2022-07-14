a9522c331d8106bc7ffda4033067f183041bf287
unknown <kamiltm@code.edu.az>
2022-07-14 20:10:15 +0400

    Address Model created

---
+++ Person Management/Person Management/Database/Models/Address.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person_Management.Database.Models
+{
+    class Address
+    {
+        private static int _idCounter;
+        public int Id { get; set; }
+
+        public string AdressName { get; set; }
+
+        public Address(string name)
+        {
+            Id = _idCounter;
+            AdressName = name;
+            _idCounter++;
+        }
+    }
+}
