98ae27b96ee80a9530cf06762e62900fabfc7db8
qafarlicavid <javidig@code.edu.az>
2022-07-14 08:48:14 -0700

    Person Model Complieted

---
+++ Person Management/Person Management/Database/Repository/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person_Management.Database.Repository
+{
+    class Person
+    {
+        private static int _idCounter;
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string FatherName { get; set; }
+        public int Fin { get; set; }
+        public string Email { get; set; }  
+
+        public Person(string name, string lastName, string fatherName, int fin, string email)
+        {
+            Id = _idCounter;
+            Name = name;
+            LastName = lastName;
+            FatherName = fatherName;
+            Fin = fin;
+            Email = email;
+
+            _idCounter++;
+        }
+    }
+}
