b28c38bc94c247ad54a2e0b7e6b88a74afa18d5d
unknown <kamiltm@code.edu.az>
2022-07-14 20:18:01 +0400

    Applied modifications to address model

--- Person Management/Person Management/Database/Models/Address.cs
+++ Person Management/Person Management/Database/Models/Address.cs
@@ -8,15 +8,15 @@ namespace Person_Management.Database.Models
 {
     class Address
     {
-        private static int _idCounter;
-        public int Id { get; set; }
+        private static int _idCounter=1;
+        public int Id { get;}
 
-        public string AdressName { get; set; }
+        public string Name { get; set; }
 
         public Address(string name)
         {
             Id = _idCounter;
-            AdressName = name;
+            Name = name;
             _idCounter++;
         }
     }
