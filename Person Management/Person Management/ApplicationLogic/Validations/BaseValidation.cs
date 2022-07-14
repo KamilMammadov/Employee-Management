d003244baeef6a3308a5a1ee41eaf50d93755005
KamilMammadov <kamiltm@code.edu.az>
2022-07-14 21:40:19 +0400

    Validations created

---
+++ Person Management/Person Management/ApplicationLogic/Validations/BaseValidation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person_Management.ApplicationLogic.Validations
+{
+    class BaseValidation
+    {
+
+        public static bool IsLength(string text,int length)
+        {
+            if (text.Length==length)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool IsContainsNumbers(string text)
+        {
+            foreach (char letter in text)
+            {
+                if (char.IsDigit(letter))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        public static bool IsLetterExist(string text,char targetLetter)
+        {
+            foreach (char letter in text)
+            {
+                if (letter==targetLetter)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+    }
+}
---
+++ Person Management/Person Management/ApplicationLogic/Validations/UserValidations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person_Management.ApplicationLogic.Validations
+{
+    class UserValidations : BaseValidation
+    {
+        public static bool IsValidations(string fin ,string email)
+        {
+            return IsFin(fin) & IsEmail(email);
+        }
+
+
+        public static bool IsFin(string fin)
+        {
+            return IsLength(fin, 7) && IsContainsNumbers(fin);
+        }
+
+        public static bool IsEmail(string email)
+        {
+            return IsLetterExist(email, '@');
+        }
+
+    }
+}
